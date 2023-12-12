3dd4a5ea2e707e8062168f5782d4dfb7cd7fca9a
elbakya <harut.elbakyan@yandex.ru>
2023-12-13 02:19:29 +0400

    Added the EKS module

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ module "vpc" {
   vpc_public_subnets = ["10.0.0.0/19","10.0.32.0/19","10.0.64.0/19"]
   vpc_private_subnets = ["10.0.128.0/19"]
 }
+
+module "eks" {
+  source = "./eks"
+  vpc_id_for_eks = module.vpc.vpc_id
+  vpc_public_subnets = module.vpc.public_subnets
+  sg_id = module.sg.security_groups
+  #depends_on = [ module.vpc ]
+}
