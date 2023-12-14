46ee3bdee87d3d937cb0863e2d7ba7d19ba253ff
elbakya <harut.elbakyan@yandex.ru>
2023-12-14 14:53:12 +0400

    Added argoCD with app

--- providers.tf
+++ providers.tf
@@ -33,3 +33,26 @@ provider "aws" {
 }
 
 
+provider "kubernetes" {
+  host = module.eks.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode (module.eks.eks_cluster_auth_data)
+  exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.eks_cluster_name]
+  }
+}
+
+provider "helm" {
+    kubernetes {
+      host = module.eks.eks_cluster_endpoint
+      cluster_ca_certificate = base64decode(module.eks.eks_cluster_auth_data)
+
+      exec {
+        api_version = "client.authentication.k8s.io/v1beta1"
+        command = "aws"
+        args = ["eks", "get-token", "--cluster-name", module.eks.eks_cluster_name]
+      }
+   }
+}
